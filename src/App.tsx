8beca75cdc30f84619ba402caa41b5086a8d8c2d
REPLYNET\d.schaly <d.schaly@reply.com>
2022-09-29 21:03:30 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { GameBanner } from './components/GameBanner';
+
+import './styles/main.css';
+
+import logoImg from './assets/logo-nlw-esports.svg';
+import { CreateAdBanner } from './components/CreateAdBanner';
+
+function App() {
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="" />
+
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <GameBanner bannerUrl='/game-1.png' title="League of Legends" adsAmount={5}/>
+      </div>
+
+      <CreateAdBanner />
+    </div> 
+  )
+}
+
+export default App
